4ec9b19802dcc13c9212ac6060c7daab186475cf
Ashley Robinson <ashley181291@gmail.com>
2018-08-05 15:32:54 +0100

    Added set and clear button to GUI

--- gui/gui.c
+++ gui/gui.c
@@ -6,29 +6,33 @@
 #define  AGENT_ID_END   10
 #define  AGENT_NUM      10
 
-static void clear_agent_check_boxes (  GtkWidget   *button,  
-                                       gpointer    *check   ){
-   
-   int         i;
-   GtkWidget   *check_widget; 
-   for(i=0;i<(AGENT_ID_END+1);i++){
-      check_widget = (GtkWidget *)check;
-      gtk_check_button_set_active
-      *check_widget.set_active((gboolean)0);
-      *check++;
-   }
+/* Clear a toggle box callback*/
+static void clear_agent_toggle_box_cb (  GtkButton   *button,  
+                                          gpointer    check   
+){ 
+   gtk_toggle_button_set_active((GtkToggleButton *)check, FALSE); 
 }
 
+/* Set a toggle box callback*/
+static void set_agent_toggle_box_cb (  GtkButton   *button,  
+                                       gpointer    check   
+){ 
+   gtk_toggle_button_set_active((GtkToggleButton *)check, TRUE); 
+}
+
+/* A widgets and main functions */
 static void activate (  GtkApplication *app,
-                        gpointer        user_data){
+                        gpointer        user_data
+){
    int         i;
    char        str[32];
    GtkWidget   *window,
                *top_box,
-               *agent_check_button_box,
-               *agent_check_button[AGENT_NUM],
-               *clear_button_box,
-               *clear_button;
+               *agent_toggle_button_box,
+               *agent_toggle_button[AGENT_NUM],
+               *all_button_box,
+               *all_clear_button,
+               *all_set_button;
 
    window = gtk_application_window_new (app);
    gtk_window_set_title (GTK_WINDOW (window), APP_NAME);
@@ -38,27 +42,34 @@ static void activate (  GtkApplication *app,
    top_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
    gtk_container_add (GTK_CONTAINER (window), top_box); 
 
-   // Add all the agent check buttons in a box
-   agent_check_button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
-   gtk_container_add (GTK_CONTAINER (top_box), agent_check_button_box); 
+   // Add all the agent toggle buttons in a box
+   agent_toggle_button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
+   gtk_container_add (GTK_CONTAINER (top_box), agent_toggle_button_box); 
    for(i=0;i<(AGENT_ID_END+1);i++){ 
       sprintf(str, "%d", i);
-      agent_check_button[i] = gtk_check_button_new_with_label (str);
-      gtk_container_add (GTK_CONTAINER (agent_check_button_box), agent_check_button[i]);
+      agent_toggle_button[i] = gtk_toggle_button_new_with_label (str);
+      gtk_container_add (GTK_CONTAINER (agent_toggle_button_box), agent_toggle_button[i]);
    }
   
-   // Add clear check boxes button
-   clear_button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
-   gtk_container_add (GTK_CONTAINER (top_box), clear_button_box); 
-   clear_button = gtk_button_new_with_label ("Clear agents");
-   g_signal_connect_swapped (clear_button, "clicked", G_CALLBACK (clear_agent_check_boxes), (gpointer)agent_check_button);
-   gtk_container_add (GTK_CONTAINER (clear_button_box), clear_button);
+   // Add set/clear all toggle buttons box
+   all_button_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
+   gtk_container_add (GTK_CONTAINER (top_box), all_button_box); 
+   all_clear_button = gtk_button_new_with_label ("Clear all agents");
+   all_set_button = gtk_button_new_with_label ("Set all agents");
+   for(i=0;i<(AGENT_ID_END+1);i++){
+      g_signal_connect(all_clear_button, "clicked", G_CALLBACK (clear_agent_toggle_box_cb), agent_toggle_button[i]);
+      g_signal_connect(all_set_button, "clicked", G_CALLBACK (set_agent_toggle_box_cb), agent_toggle_button[i]);
+   }
+   gtk_container_add (GTK_CONTAINER (all_button_box), all_clear_button);
+   gtk_container_add (GTK_CONTAINER (all_button_box), all_set_button);
 
+   
    gtk_widget_show_all (window);
 }
 
 int main (  int    argc,
-            char **argv){
+            char **argv
+){
    GtkApplication *app;
    int status;
    
