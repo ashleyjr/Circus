a5a7d047dc140af2a888d3c2bcc8eb0bb31a23c4
Ashley Robinson <ashley181291@gmail.com>
2018-08-07 21:39:41 +0100

    Added timer for buffer

--- gui/gui.c
+++ gui/gui.c
@@ -5,6 +5,36 @@
 #define  AGENT_ID_START 1
 #define  AGENT_ID_END   10
 #define  AGENT_NUM      10
+#define  BUFFER_TIME_S  0.011
+#define  BUFFER_TX_SIZE 128
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+
+static volatile uint8_t    tx_buffer[BUFFER_TX_SIZE];
+static volatile uint16_t   tx_buffer_head,
+                           tx_buffer_tail;
+
+/* Timer function called to unload the buffer
+   and transmit data */
+static gboolean unload_tx_buffer( gpointer textview  
+){
+   if(tx_buffer_head != tx_buffer_tail){ 
+      sprintf(str, "%d", tx_buffer[tx_buffer_tail]); 
+      // TODO: Send data
+      tx_buffer_tail++; 
+      tx_buffer_tail %= BUFFER_TX_SIZE;
+   } 
+   return TRUE;
+}
+
+/* Called by user and loads the buffer */
+static void load_tx_buffer(   uint8_t data 
+){
+   tx_buffer[tx_buffer_head] = data;
+   tx_buffer_head++;
+   tx_buffer_head %= BUFFER_TX_SIZE;
+}
 
 /* Clear a toggle box callback*/
 static void clear_agent_toggle_box_cb (  GtkButton   *button,  
@@ -20,6 +50,14 @@ static void set_agent_toggle_box_cb (  GtkButton   *button,
    gtk_toggle_button_set_active((GtkToggleButton *)check, TRUE); 
 }
 
+/* Key press callback for all active keys */
+static void  keypress_cb (GtkWidget *widget, GdkEventKey *event, gpointer data) {
+   switch (event->keyval){
+      case GDK_KEY_Left:   load_tx_buffer('A');                          
+                           break; 
+   }      
+}
+
 /* A widgets and main functions */
 static void activate (  GtkApplication *app,
                         gpointer        user_data
@@ -32,7 +70,9 @@ static void activate (  GtkApplication *app,
                *agent_toggle_button[AGENT_NUM],
                *all_button_box,
                *all_clear_button,
-               *all_set_button;
+               *all_set_button,
+               *textview_box,
+               *textview;
 
    window = gtk_application_window_new (app);
    gtk_window_set_title (GTK_WINDOW (window), APP_NAME);
@@ -63,7 +103,20 @@ static void activate (  GtkApplication *app,
    gtk_container_add (GTK_CONTAINER (all_button_box), all_clear_button);
    gtk_container_add (GTK_CONTAINER (all_button_box), all_set_button);
 
-   
+   // Add the text view  
+   textview_box = gtk_button_box_new (GTK_ORIENTATION_VERTICAL);
+   gtk_container_add (GTK_CONTAINER (top_box), textview_box); 
+   textview = gtk_text_view_new ();
+   gtk_container_add (GTK_CONTAINER (textview_box), textview);
+
+   // Key press events
+   gtk_widget_add_events(window, GDK_KEY_PRESS_MASK);
+   g_signal_connect (G_OBJECT (window), "key_press_event", G_CALLBACK (keypress_cb), NULL);
+
+   // Connect timer for transmit
+   g_timeout_add_seconds(BUFFER_TIME_S, unload_tx_buffer, textview);
+
+   // Added
    gtk_widget_show_all (window);
 }
 
@@ -73,6 +126,11 @@ int main (  int    argc,
    GtkApplication *app;
    int status;
    
+   // Init buffer
+   tx_buffer_head = 0;
+   tx_buffer_tail = 0;
+
+   // Init app
    app = gtk_application_new (APP_ID, G_APPLICATION_FLAGS_NONE);
    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
    status = g_application_run (G_APPLICATION (app), argc, argv);
