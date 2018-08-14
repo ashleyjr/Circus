1d92bc202cfb6c5f04a99bacd2963e3fb7ed3171
Ashley Robinson <ashley181291@gmail.com>
2018-08-14 08:30:23 +0100

    new serial interface

--- gui/gui.c
+++ gui/gui.c
@@ -1,3 +1,4 @@
+/* Include */
 #include <gtk/gtk.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,75 +7,69 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
 
-#define BAUDRATE B115200 
-#define MODEMDEVICE "/dev/ttyUSB0"
+/* Define */
+#define  BAUDRATE       B115200 
+#define  PORTNAME       "/dev/ttyUSB0"
 #define  APP_ID         "org.gtk.circus"
 #define  APP_NAME       "Circus"
 #define  AGENT_ID_START 1
 #define  AGENT_ID_END   10
 #define  AGENT_NUM      10
-#define  BUFFER_TIME_S  0.011
+#define  BUFFER_TIME_S  0.01
 #define  BUFFER_TX_SIZE 128
 
-int fd=0;
-struct termios oldtp, newtp;
+/* Typedefs */
+typedef  unsigned char   uint8_t;
+typedef  unsigned short  uint16_t;
 
-typedef unsigned char   uint8_t;
-typedef unsigned short  uint16_t;
-
-static volatile uint8_t    tx_buffer[BUFFER_TX_SIZE];
-static volatile uint16_t   tx_buffer_head,
-                           tx_buffer_tail;
-
-
-void openport( void
+/* Globals */
+static   volatile int               fd;
+static   volatile uint8_t           tx_buffer[BUFFER_TX_SIZE];
+static   volatile uint16_t          tx_buffer_head,
+                                    tx_buffer_tail;
+/* Open a port to the UART */
+int openport (    int fd, 
+                  int speed, 
+                  int parity
 ){
-   fd = open(MODEMDEVICE, O_RDWR | O_NOCTTY |O_NDELAY );	
-   if (fd < 0){
-      perror(MODEMDEVICE);         
-   }                                                         
-   fcntl(fd,F_SETFL,0);
-   tcgetattr(fd,&oldtp); /* save current serial port settings */
-   // tcgetattr(fd,&newtp); /* save current serial port settings */
-   bzero(&newtp, sizeof(newtp));
-   // bzero(&oldtp, sizeof(oldtp));
-   
-   newtp.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
-   
-   newtp.c_iflag = IGNPAR | ICRNL;
-   
-   newtp.c_oflag = 0;
-   
-   newtp.c_lflag = ICANON;
-   
-   newtp.c_cc[VINTR]    = 0;     /* Ctrl-c */
-   newtp.c_cc[VQUIT]    = 0;     /* Ctrl-\ */
-   newtp.c_cc[VERASE]   = 0;     /* del */
-   newtp.c_cc[VKILL]    = 0;     /* @ */
-   //newtp.c_cc[VEOF]     = 4;     /* Ctrl-d */
-   newtp.c_cc[VEOF]     = 0;     /* Ctrl-d */
-   newtp.c_cc[VTIME]    = 0;     /* inter-character timer unused */
-   newtp.c_cc[VMIN]     = 1;     /* blocking read until 1 character arrives */
-   newtp.c_cc[VSWTC]    = 0;     /* '\0' */
-   newtp.c_cc[VSTART]   = 0;     /* Ctrl-q */
-   newtp.c_cc[VSTOP]    = 0;     /* Ctrl-s */
-   newtp.c_cc[VSUSP]    = 0;     /* Ctrl-z */
-   newtp.c_cc[VEOL]     = 0;     /* '\0' */
-   newtp.c_cc[VREPRINT] = 0;     /* Ctrl-r */
-   newtp.c_cc[VDISCARD] = 0;     /* Ctrl-u */
-   newtp.c_cc[VWERASE]  = 0;     /* Ctrl-w */
-   newtp.c_cc[VLNEXT]   = 0;     /* Ctrl-v */
-   newtp.c_cc[VEOL2]    = 0;     /* '\0' */ 
+   struct termios tty;
+   memset (&tty, 0, sizeof tty);
+   if (tcgetattr (fd, &tty) != 0){ 
+      return -1;
+   }
+   cfsetospeed (&tty, speed);
+   cfsetispeed (&tty, speed);
+   tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
+   //disable IGNBRK for mismatched speed tests; otherwise receive break
+   // as \000 chars
+   tty.c_iflag &= ~IGNBRK;                         // disable break processing
+   tty.c_lflag = 0;                                // no signaling chars, no echo,
+   // no canonical processing
+   tty.c_oflag = 0;                                // no remapping, no delays
+   tty.c_cc[VMIN]  = 0;                            // read doesn't block
+   tty.c_cc[VTIME] = 5;                            // 0.5 seconds read timeout 
+   tty.c_iflag &= ~(IXON | IXOFF | IXANY);         // shut off xon/xoff ctrl
+   tty.c_cflag |= (CLOCAL | CREAD);                // ignore modem controls,
+   // enable reading
+   tty.c_cflag &= ~(PARENB | PARODD);              // shut off parity
+   tty.c_cflag |= parity;
+   tty.c_cflag &= ~CSTOPB;
+   tty.c_cflag &= ~CRTSCTS; 
+   if (tcsetattr (fd, TCSANOW, &tty) != 0){ 
+      return -1;
+   }
+   return 0;
 }
-
 /* Timer function called to unload the buffer
    and transmit data */
 static gboolean unload_tx_buffer( gpointer textview  
 ){
    char str[2];
-
-   if(tx_buffer_head != tx_buffer_tail){ 
+   
+   while(tx_buffer_head != tx_buffer_tail){ 
       sprintf(str, "%c", tx_buffer[tx_buffer_tail]);
       g_print(str);
       write(fd, str, 2);
@@ -187,7 +182,10 @@ int main (  int    argc,
    tx_buffer_tail = 0;
 
    // Connect UART
-   openport();
+   fd = open (PORTNAME, O_RDWR | O_NOCTTY | O_SYNC);
+   if((fd < 0) || (openport(fd, BAUDRATE, 0) < 0)){
+      printf("\n\rUnable to open serial port\n\r"); 
+   }
 
    // Init app
    app = gtk_application_new (APP_ID, G_APPLICATION_FLAGS_NONE);
